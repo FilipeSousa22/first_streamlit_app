d7c2ba2ba687f0064b3389c986ad76bb0c5868f5
Filipe Sousa <65247512+FilipeSousa22@users.noreply.github.com>
2023-09-25 15:46:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-st.dataframe(my_fruit_list)
+
 
 st.title('My Parents new healthy Diner')
 
@@ -11,3 +10,9 @@ st.text('🥣Omega 3 & Blueberry Oatmeal')
 st.text('🥗Kale, Spinach & Rocket Smoothie')
 st.text('🐔Hard-Boiled Free-Range Egg') 
 st.text('🥑🍞 Avocado Toast')
+
+
+---------------  Table #import  ------------------------------------------------
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
