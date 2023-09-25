38c4022e00a56595083fb5cae5c41e6a1404ab3d
Filipe Sousa <65247512+FilipeSousa22@users.noreply.github.com>
2023-09-25 15:47:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.text('🐔Hard-Boiled Free-Range Egg')
 st.text('🥑🍞 Avocado Toast')
 
 
------------------------------------------------  //  --------------------------------------------------
+-----------------------------------------------   --------------------------------------------------
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 st.dataframe(my_fruit_list)
