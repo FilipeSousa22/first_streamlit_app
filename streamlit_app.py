8ee6057f8ecd82820c9624222710b90aeba451e7
Filipe Sousa <65247512+FilipeSousa22@users.noreply.github.com>
2023-09-25 14:22:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('My Parents new healthy Diner')
 
-st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Omega 3 & Blueberry Oatmeal' \n 'Kale, Spinach & Rocket Smoothie')
